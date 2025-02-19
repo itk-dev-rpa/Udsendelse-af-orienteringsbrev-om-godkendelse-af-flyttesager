31306c80a8f074a4243625c94153fc91eb11d725
kri-bak <kriba@aarhus.dk>
2025-02-19 12:04:57 +0100

    Updated KLE and headline according to change from BO

--- robot_framework/config.py
+++ robot_framework/config.py
@@ -32,8 +32,8 @@ CASEWORKER = Caseworker(
         uuid='203e17a1-0032-4f1d-be98-9386e4f2f336'
 )
 
-CASE_HEADLINE = "Folkeregistrering i almindelighed"
-KLE = "23.05.00"
+CASE_HEADLINE = "Flytteanmeldelse-Godkendt"
+KLE = "23.05.10"
 PROCEEDING_FACET = "G01"
 SENSITIVITY = "Fortrolige"
 
